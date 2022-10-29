1eb1f69b4798f7ab80d45a966a63d44a5c1541df
Furkan <furkanuzundev@gmail.com>
2022-10-30 00:37:15 +0300

    Update Campaings.tsx

--- src/screens/Campaings.tsx
+++ src/screens/Campaings.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 interface CampaingsProps {}
 
@@ -7,6 +7,7 @@ const Campaings = (props: CampaingsProps) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Campaings</Text>
+      <Text style={styles.text}>1</Text>
     </View>
   );
 };
@@ -16,11 +17,11 @@ export default Campaings;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
